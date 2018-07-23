bd6c483946014e8adc80ccff79d9c74f3d705db9
chenguose <12345678>
2018-07-23 10:04:47 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <HelloWorld1/>
+    <HelloWorld3/>
+    <aa></aa>
+  </div>
+</template>
+
+<script>
+import HelloWorld1 from './components/HelloWorld'
+import {HelloWorld ,HelloWorld3}from './components/index'
+let aa = () => import('./components/HelloWorld1')
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,HelloWorld1,aa,HelloWorld3
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import HelloWorld from './HelloWorld'
+const HelloWorld3 = ()=>import('./HelloWorld2.vue')
+export {
+  HelloWorld,HelloWorld3
+}
